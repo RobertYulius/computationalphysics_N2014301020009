cd78fe36ee2eed7ccbf5dee944222b8a6b2d1061
Jiangwei Lu <xavii.l@outlook.com>
2016-09-14 00:50:29 +0800

    Create Neme.py
    只 print 自己的名字没啥意思就随手做了个字符画_(:з」∠)_

---
+++ Neme.py
@@ -0,0 +1,44 @@
+import time
+#用字符画的方式<del>虚区<del>输出自己的名字，逐行打印并添加延迟。
+print' gga'
+time.sleep(0.5)
+print' ggg                           eg   ae                                                                eg'
+time.sleep(0.5)
+print' eaa'
+time.sleep(0.5)
+print' aaa      ig     ea            eg   ga    ggaaega   geieaggi     aagaiea   gii   eag   aea  gieggai   gg'
+time.sleep(0.5)
+print' gia      ga     gg            ge   ig         ga   ei     ei   ig     gg   ee   agg   eg  ee     ai  ai'
+time.sleep(0.5)
+print' egg      gg     ai            ia   ge    egeeaii   ag     aa  gi      gg    ag ig ig ei   iggiieegi  ga'
+time.sleep(0.5)
+print' aai      gg     gi           gi    ga    ai   ig   ae     ai  ag      ge    giii   egg    eg         ii'
+time.sleep(0.5)
+print' iegaggi   eagggae         aaag     ig    gaeei ig  aa     eg   igegiggig     ie     ag     eigeeeg   ga'
+time.sleep(0.5)
+print'                                                                       ig'
+time.sleep(0.5)
+print'                                                               gi     ig'
+time.sleep(0.5)
+print'                                                                 egggeg'
+time.sleep(0.5)
+print''
+time.sleep(0.5)
+print''
+time.sleep(0.5)
+print'                                                                                         Made by N'
+#  gga
+#  ggg                           eg   ae                                                                eg
+#  eaa
+#  aaa      ig     ea            eg   ga    ggaaega   geieaggi     aagaiea   gii   eag   aea  gieggai   gg
+#  gia      ga     gg            ge   ig         ga   ei     ei   ig     gg   ee   agg   eg  ee     ai  ai
+#  egg      gg     ai            ia   ge    egeeaii   ag     aa  gi      gg    ag ig ig ei   iggiieegi  ga
+#  aai      gg     gi           gi    ga    ai   ig   ae     ai  ag      ge    giii   egg    eg         ii
+#  iegaggi   eagggae         aaag     ig    gaeei ig  aa     eg   igegiggig     ie     ag     eigeeeg   ga
+#                                                                        ig
+#                                                                gi     ig
+#                                                                  egggeg
+# 
+# 
+#                                                                                          Made by N
+
